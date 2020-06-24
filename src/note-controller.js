6c86a5c91b22c248b9064fa09c08451d0649fae6
victorvallet <victorvallet8@gmail.com>
2020-06-24 17:36:02 +0200

    Passed the last step to change our html element

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,4 +1,21 @@
-var ele = document.getElementById('app')
-ele.innerHTML = "howdy";
+
+
+(function (exports) {
+
+    function NoteController(noteListModel) {
+        noteListModel = new NoteList();
+        noteListModel.addNote("Favourite drink: seltzer");
+
+        this.view = new NoteListView(noteListModel)
+    }
+
+    NoteController.prototype.getHTML = function () {
+        var ele = document.getElementById('app')
+        ele.innerHTML = this.view.returnHTML();
+    }
+
+
+    exports.NoteController = NoteController;
+})(this);
 
 
