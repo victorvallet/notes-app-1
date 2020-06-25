14d85e561183afa90dea0f35d42faedd250393ec
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-25 14:54:50 +0100

    refactored code from step 3 to only output 20 chars per string

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,25 +1,20 @@
-
 (function (exports) {
-    function NoteListView(notelist) {
-        this.frame = notelist;
+  function NoteListView(notelist) {
+    this.frame = notelist;
+  }
+
+  NoteListView.prototype.returnHTML = function () {
+    var array = this.frame.returnNote();
+    var output = "";
+
+    if (array.length === 0) {
+      return "<ul><li><div></div></li></ul>";
+    } else {
+      array.forEach((note) => {
+        output += `<li><div>${note.returnText().substr(0, 20)}</div></li>`;
+      });
+      return `<ul>${output}</ul>`;
     }
-
-    NoteListView.prototype.returnHTML = function () {
-        var array = this.frame.returnNote()
-        var output = ""
-
-        if (array.length === 0) {
-            return "<ul><li><div></div></li></ul>"
-        }
-
-        else {
-            array.forEach((note) => {
-                output += `<li><div>${note.returnText()}</div></li>`
-            })
-            return `<ul>${output}</ul>`
-        }
-
-    }
-    exports.NoteListView = NoteListView;
+  };
+  exports.NoteListView = NoteListView;
 })(this);
-
--- tests/note-list-view-test.js
+++ tests/note-list-view-test.js
@@ -1,32 +1,34 @@
-
 function constructsWithAnNoteListModel() {
-    let list = new NoteList();
-    let view = new NoteListView(list);
-    expect.isTrue(view.frame instanceof NoteList);
+  let list = new NoteList();
+  let view = new NoteListView(list);
+  expect.isTrue(view.frame instanceof NoteList);
 }
 constructsWithAnNoteListModel();
 
 function returnHTMLStringNoNote() {
-    let list = new NoteList();
-    let view = new NoteListView(list);
-    expect.isTrue(view.returnHTML() === "<ul><li><div></div></li></ul>")
+  let list = new NoteList();
+  let view = new NoteListView(list);
+  expect.isTrue(view.returnHTML() === "<ul><li><div></div></li></ul>");
 }
 returnHTMLStringNoNote();
 
 function returnHTMLStringOneNote() {
-    let list = new NoteList();
-    let view = new NoteListView(list);
-    list.addNote("Rae");
-    expect.isTrue(view.returnHTML() === "<ul><li><div>Rae</div></li></ul>")
+  let list = new NoteList();
+  let view = new NoteListView(list);
+  list.addNote("This is a string that I have put together");
+  expect.isTrue(
+    view.returnHTML() === "<ul><li><div>This is a string tha</div></li></ul>"
+  );
 }
 returnHTMLStringOneNote();
 
 function returnHTMLStringSeveralNotes() {
-    let list = new NoteList();
-    let view = new NoteListView(list);
-    list.addNote("Rae");
-    list.addNote("Minsi");
-    var string = "<ul><li><div>Rae</div></li><li><div>Minsi</div></li></ul>"
-    expect.isTrue(view.returnHTML() === string)
+  let list = new NoteList();
+  let view = new NoteListView(list);
+  list.addNote("This is a string that I have put together");
+  list.addNote("This is a string that I have put together");
+  var string =
+    "<ul><li><div>This is a string tha</div></li><li><div>This is a string tha</div></li></ul>";
+  expect.isTrue(view.returnHTML() === string);
 }
 returnHTMLStringSeveralNotes();
