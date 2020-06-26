2d90b88171314149ca9d83b62a6c0b1f4580e8df
victorvallet <victorvallet8@gmail.com>
2020-06-26 16:54:03 +0200

    Finished step13

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,40 +1,41 @@
+
+
 (function (exports) {
-  function NoteController(noteListModel) {
-    this.noteListModel = new NoteList();
-    this.noteListModel.addNote('Favourite drink: seltzer');
-    this.noteListModel.addNote('Favourite drink: beer');
-    this.noteListModel.addNote('Favourite drink: cola');
+    function NoteController(noteListModel) {
+        this.noteListModel = new NoteList();
 
-    this.view = new NoteListView(this.noteListModel);
-  }
+        this.view = new NoteListView(this.noteListModel);
+    }
 
-  NoteController.prototype.getHTML = function () {
-    var ele = document.getElementById('app');
-    ele.innerHTML = this.view.returnHTML();
-  };
+    NoteController.prototype.getHTML = function () {
+        var ele = document.getElementById('app');
+        ele.innerHTML = this.view.returnHTML();
+    };
 
-  exports.NoteController = NoteController;
+    exports.NoteController = NoteController;
 })(this);
 
 window.addEventListener(`DOMContentLoaded`, function () {
-  var notelist = new NoteList();
-  var controller = new NoteController(notelist);
+    var notelist = new NoteList();
+    var controller = new NoteController(notelist);
 
-  controller.getHTML();
+    controller.getHTML();
 
-  window.addEventListener('hashchange', function () {
-    let noteIndex = window.location.hash.slice(-1);
-    let note = controller.noteListModel.returnNote()[noteIndex];
-    console.log(noteIndex);
-    var singlenote = new SingleNote(note);
+    window.addEventListener('hashchange', function () {
+        let noteIndex = window.location.hash.slice(-1);
+        let note = controller.noteListModel.returnNote()[noteIndex];
+        console.log(noteIndex);
+        var singlenote = new SingleNote(note);
 
-    var element = document.getElementById('app');
-    element.innerHTML = singlenote.returnsHTML();
-  });
+        var element = document.getElementById('app');
+        element.innerHTML = singlenote.returnsHTML();
+    });
 
-  document.getElementById('form').onsubmit = function (submitEvent) {
-    submitEvent.preventDefault();
-    console.log(document.getElementById('form').elements[0].value);
-    console.log(submitEvent);
-  };
+    document.getElementById('form').onsubmit = function (submitEvent) {
+        submitEvent.preventDefault();
+        let note = document.getElementById('form').elements[0].value;
+        controller.noteListModel.addNote(note);
+        controller.getHTML();
+        console.log(submitEvent);
+    };
 });
