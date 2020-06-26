6b783ce6a9d900e3c1391d30a6c7644780eaa42b
victorvallet <victorvallet8@gmail.com>
2020-06-26 15:46:48 +0200

    Finished step9 to display the full note

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,41 +1,66 @@
+
+
 (function (exports) {
-  function NoteController(noteListModel) {
-    this.noteListModel = new NoteList();
-    this.noteListModel.addNote("Favourite drink: seltzer");
-    this.noteListModel.addNote("Favourite drink: beer");
-    this.noteListModel.addNote("Favourite drink: cola");
+    function NoteController(noteListModel) {
+        this.noteListModel = new NoteList();
+        this.noteListModel.addNote("Favourite drink: seltzer");
+        this.noteListModel.addNote("Favourite drink: beer");
+        this.noteListModel.addNote("Favourite drink: cola");
 
-    this.view = new NoteListView(this.noteListModel);
-  }
+        this.view = new NoteListView(this.noteListModel);
+    }
 
-  NoteController.prototype.getHTML = function () {
-    var ele = document.getElementById("app");
-    ele.innerHTML = this.view.returnHTML();
-  };
+    NoteController.prototype.getHTML = function () {
+        var ele = document.getElementById("app");
+        ele.innerHTML = this.view.returnHTML();
+    };
 
-  NoteController.prototype.loadNote = function () {
-    window.addEventListener("hashchange", this.addNoteToHTML);
-  };
+    // NoteController.prototype.returnNumber = function () {
+    //     return window.location.hash.split("#")[1];
+    // };
 
-  NoteController.prototype.getNumber = function (location) {
-    return location.hash.split("#")[1];
-  };
+    // NoteController.prototype.addNoteToHTML = function () {
+    //     let noteId = window.location.hash.split("#")[1];
+    //     console.log(noteId);
+    //     let singleNote = new SingleNote(this.noteListModel.getNote(0));
+    //     console.log(singleNote)
+    //     getNoteFromList(noteId);
+    // };
 
-  NoteController.prototype.addNoteToHTML = function () {
-    let noteId = this.getNumber(window.location);
-    console.log(noteId);
-    this.getNoteFromList(noteId);
-  };
+    // NoteController.prototype.getNoteFromList = function (noteId) {
+    //     let singleNote = new SingleNote(this.noteListModel.getNote(noteId));
+    //     console.log(singleNote)
+    //     this.displayNote(singleNote);
+    // };
 
-  NoteController.prototype.getNoteFromList = function (noteId) {
-    let singleNote = new SingleNote(this.noteListModel.getNote(noteId));
-    this.displayNote(singleNote);
-  };
+    // NoteController.prototype.displayNote = function (note) {
+    //     let ele = document.getElementById("app");
+    //     ele.innerHTML = note.returnHTML();
+    // };
 
-  NoteController.prototype.displayNote = function (note) {
-    let ele = document.getElementById("app");
-    ele.innerHTML = note.returnHTML();
-  };
+    // NoteController.prototype.loadNote = function () {
+    //     window.addEventListener("hashchange", this.addNoteToHTML);
+    // };
 
-  exports.NoteController = NoteController;
+
+    exports.NoteController = NoteController;
 })(this);
+
+window.addEventListener(`DOMContentLoaded`, function () {
+    var notelist = new NoteList();
+    var controller = new NoteController(notelist);
+
+    controller.getHTML();
+
+    window.addEventListener("hashchange", function () {
+        let noteIndex = window.location.hash.slice(-1);
+        let note = controller.noteListModel.returnNote()[noteIndex]
+        console.log(noteIndex)
+        var singlenote = new SingleNote(note);
+
+        var element = document.getElementById("app");
+        element.innerHTML = singlenote.returnsHTML();
+
+    })
+
+})
